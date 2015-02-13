8a1cc8e293108db93b148664cbf1dde6c9ae2f52
orizion <stefan.mettler92@gmail.com>
2015-02-13 19:41:48 +0100

    Project Setup complete, first commit

---
+++ desktop/src/ch/games/roguepg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.games.roguepg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.games.roguepg.game.RoguePG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RoguePG(), config);
+	}
+}
