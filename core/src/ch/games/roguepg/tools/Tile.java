b167951f5dea9959b1c5d25ab20a8cff580521ec
orizion <stefan.mettler92@gmail.com>
2015-03-21 16:49:53 +0100

    Former Push invalid, correct Push following now

--- core/src/ch/games/roguepg/tools/Tile.java
+++ core/src/ch/games/roguepg/tools/Tile.java
@@ -6,6 +6,13 @@ import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.MapRenderer;
 
+/**
+ * 
+ * @author Stefan
+ * Class which holds the Texture and the Type of a tile to identify and render it in the game
+ * A Tile object represents a type of tile which is to use for a map, not an individual tile, therefore
+ * one should only create one Tile object instance per type of Tile to use 
+ */
 public class Tile extends MapObject{
 	public static enum TileType {
 		GRASS,
