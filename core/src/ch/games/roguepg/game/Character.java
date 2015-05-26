4e233b0fb35d04145fc8d68c95e71dc3547b7d68
PaEl94 <pascal.ellenberger@openmailbox.org>
2015-05-26 16:26:04 +0200

    Moved camera into Character.

--- core/src/ch/games/roguepg/game/Character.java
+++ core/src/ch/games/roguepg/game/Character.java
@@ -1,87 +1,96 @@
-package ch.games.roguepg.game;                                                                                                             
+package ch.games.roguepg.game;
 
 import ch.games.roguepg.tools.RogueMap;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import java.util.Random;
 
-public class Character extends Actor{
+public class Character extends Actor {
+
     private final Texture sprite;
     private final int speed;
-    public Character(){
-        sprite =  new Texture("player.gif");
-        setBounds(RogueMap.tileIndices.length*64/2, RogueMap.tileIndices[0].length*64/2, sprite.getWidth(), sprite.getHeight());  
+    private final OrthographicCamera camera;
+
+    public Character() {
+        sprite = new Texture("player.gif");
+        setBounds(
+                RogueMap.tileIndices.length * RogueMap.TILE_SIZE / 2, 
+                RogueMap.tileIndices[0].length * RogueMap.TILE_SIZE / 2,
+                sprite.getWidth(), 
+                sprite.getHeight()
+        );
         Random rnd = new Random();
-        while (RogueMap.tileIndices[RogueMap.coordToIndex(getX())][RogueMap.coordToIndex(getY())] == 0){
-           setX(rnd.nextFloat()*(64*64));
-           setY(rnd.nextFloat()*(64*64));
+        while (RogueMap.tileIndices[RogueMap.coordToIndex(getX())][RogueMap.coordToIndex(getY())] == 0) {
+            setPosition(
+                    rnd.nextFloat() * (RogueMap.tileIndices.length * RogueMap.TILE_SIZE), 
+                    rnd.nextFloat() * (RogueMap.tileIndices[0].length * RogueMap.TILE_SIZE)
+            );
         }
-
         speed = 500;
+        camera = new OrthographicCamera(1920, 1080);
     }
 
     @Override
-    public void draw (Batch batch, float parentAlpha) {
+    public void draw(Batch batch, float parentAlpha) {
         batch.draw(sprite, getX(), getY());
-    }
-
-    @Override
-    protected void positionChanged(){
-        /* Make camera follow player */
-        RoguePG.camera.position.set(getX() + getHeight() / 2, getY() + getWidth() / 2, 0);
     }
 
     @Override
     public void act(float delta) {
         float oldX = getX();
         float oldY = getY();
-        
+
         /* Capture Input for movement */
-        //TODO: convert to actions
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)
-            | Gdx.input.isKeyPressed(Input.Keys.A)) {
+                | Gdx.input.isKeyPressed(Input.Keys.A)) {
             setX(getX() - speed * Gdx.graphics.getDeltaTime());
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)
-            | Gdx.input.isKeyPressed(Input.Keys.D)) {
+                | Gdx.input.isKeyPressed(Input.Keys.D)) {
             setX(getX() + speed * Gdx.graphics.getDeltaTime());
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)
-           | Gdx.input.isKeyPressed(Input.Keys.W)){
+                | Gdx.input.isKeyPressed(Input.Keys.W)) {
             setY(getY() + speed * Gdx.graphics.getDeltaTime());
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)
-           | Gdx.input.isKeyPressed(Input.Keys.S)){
+                | Gdx.input.isKeyPressed(Input.Keys.S)) {
             setY(getY() - speed * Gdx.graphics.getDeltaTime());
         }
-        if(isCollided()){
+        if (isCollided()) {
             setX(oldX);
             setY(oldY);
         }
-    }   
-        
-    public boolean isCollided(){
-        /* Check only positions around player for collision */
-        /*future: Check list of impassable (eg. is 1, 2, 54 or 89) / have second tileIndices be generated based on that list.*/
-        //check for direction, so only one coordinate gets reset to oldX/Y --> movement along walls
-        float x = getX(); 
-        float y = getY();
-     
-       if(RogueMap.tileIndices[RogueMap.coordToIndex(x+20)][RogueMap.coordToIndex(y+10)] == 0){ //bottom left corner
-           return true;
-       }
-       else if(RogueMap.tileIndices[RogueMap.coordToIndex(x+40)][RogueMap.coordToIndex(y+10)] == 0){
-           return true;
-       }      
-       else if(RogueMap.tileIndices[RogueMap.coordToIndex(x+20)][RogueMap.coordToIndex(y+40)] == 0){
-           return true;
-       }
-        else if(RogueMap.tileIndices[RogueMap.coordToIndex(x+40)][RogueMap.coordToIndex(y+40)] == 0){
-           return true;
-       }
-       else{ return false; }
+        /* Make camera follow player */
+        camera.position.set(getX() + getHeight() / 2, getY() + getWidth() / 2, 0);
     }
-}                    
+
+    public boolean isCollided() {
+        /* Check only positions around player for collision */
+        float x = getX();
+        float y = getY();
+        try {
+            if (RogueMap.tileIndices[RogueMap.coordToIndex(x + 20)][RogueMap.coordToIndex(y + 10)] == 0) {
+                return true;
+            } else if (RogueMap.tileIndices[RogueMap.coordToIndex(x + 40)][RogueMap.coordToIndex(y + 10)] == 0) {
+                return true;
+            } else if (RogueMap.tileIndices[RogueMap.coordToIndex(x + 20)][RogueMap.coordToIndex(y + 40)] == 0) {
+                return true;
+            } else if (RogueMap.tileIndices[RogueMap.coordToIndex(x + 40)][RogueMap.coordToIndex(y + 40)] == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return true;
+        }
+    }
+
+    OrthographicCamera getCamera() {
+        return camera;
+    }
+}
--- core/src/ch/games/roguepg/game/RoguePG.java
+++ core/src/ch/games/roguepg/game/RoguePG.java
@@ -4,39 +4,31 @@ import ch.games.roguepg.tools.RogueMap;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class RoguePG extends Game {
-    public static SpriteBatch batch;
-    private Character player;
-    private Stage world;
-    static OrthographicCamera camera;
     private RogueMap rogueMap;
+    private Character player;
+    private SpriteBatch batch;
     private ScreenViewport viewport;
-
+    private Stage world;
 
     @Override
-    public void create () {
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1920, 1080);
-        batch = new SpriteBatch();
-        batch.setProjectionMatrix(camera.combined);
-        viewport = new ScreenViewport(camera);
+    public void create() {
         rogueMap = new RogueMap(64.0f, 64, 64);
         player = new Character();
+        batch = new SpriteBatch();
+        batch.setProjectionMatrix(player.getCamera().combined);
+        viewport = new ScreenViewport(player.getCamera());
         world = new Stage(viewport, batch);
-        Gdx.input.setInputProcessor(world);
         world.addActor(rogueMap);
         world.addActor(player);
-        camera.position.set(player.getX() + player.getHeight() / 2, 
-                player.getY() + player.getWidth() / 2, 0
-        );
     }
+
     @Override
-     public void render() {
-        camera.update();
+    public void render() {
         Gdx.gl.glClearColor(0, 0.5f, 0, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
         world.act(Gdx.graphics.getDeltaTime());
