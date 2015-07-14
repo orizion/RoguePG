361d85d4e4cc02f9cfbee56f8a393807eac850c1
orizion <stefan.mettler92@gmail.com>
2015-07-14 20:18:49 +0200

    Tracing Entity Spawning Bug
    Changed  Entity Positioning to use integer values
    Tracing a bug in Entity Positioning, changed the body positioning for
    walls experimentally

--- core/src/ch/games/roguepg/game/RoguePG.java
+++ core/src/ch/games/roguepg/game/RoguePG.java
@@ -1,6 +1,7 @@
 package ch.games.roguepg.game;
 
 import ch.games.roguepg.tools.RogueMap;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ai.steer.behaviors.Flee;
@@ -13,18 +14,17 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-
 public class RoguePG extends Game {
 
     public static final String TITLE = "RoguePG";
     public static final int V_WIDTH = 1920;
     public static final int V_HEIGHT = 1080;
-    public static final float PPM = 64;
+    public static final int PPM = 64;
     public static final World world = new World(new Vector2(0, 0), true);
 
     private RogueMap rogueMap;
     private Player player;
-    private Monster monster, monster1, monster2, monster3;
+    private Monster monster, monster1, monster2;
     private SpriteBatch batch;
     private ScreenViewport viewport;
     private Stage stage;
@@ -35,13 +35,12 @@ public class RoguePG extends Game {
         Gdx.input.setCursorCatched(true);
         rogueMap = new RogueMap(32, 32);
         player = new Player();
+        // Monster seeks monster1, monster1 flees and monster2 just wanders around
         monster = new Monster();
         monster1 = new Monster();
         monster2 = new Monster();
-        monster3 = new Monster();
-        monster.getSteeringAgent().setSteeringBehavior(new Seek(monster.getSteeringAgent(), monster1.getSteeringAgent()));
-        monster1.getSteeringAgent().setSteeringBehavior(new Flee(monster1.getSteeringAgent(), monster.getSteeringAgent()));
-        monster3.getSteeringAgent().setSteeringBehavior(new Seek(monster3.getSteeringAgent(), player.getSteeringAgent()));
+        monster.setSteeringBehavior(new Seek(monster, monster1));
+        monster1.setSteeringBehavior(new Flee(monster1, monster));
         batch = new SpriteBatch();
         batch.setProjectionMatrix(player.getCamera().combined);
         viewport = new ScreenViewport(player.getCamera());
@@ -51,12 +50,12 @@ public class RoguePG extends Game {
         stage.addActor(monster);
         stage.addActor(monster1);
         stage.addActor(monster2);
-        stage.addActor(monster3);
+        RogueMap.printMap();
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0.141f, 0.182f, 0.205f, 1);
+        Gdx.gl.glClearColor(0, 0.5f, 0, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
         world.step(1 / 60f, 6, 2);
         stage.act(Gdx.graphics.getDeltaTime());
--- core/src/ch/games/roguepg/tools/RogueMap.java
+++ core/src/ch/games/roguepg/tools/RogueMap.java
@@ -1,15 +1,17 @@
 package ch.games.roguepg.tools;
 
-import ch.games.roguepg.game.RoguePG;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import java.util.ArrayList;
 import java.util.Random;
+
+import ch.games.roguepg.game.RoguePG;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.ChainShape;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class RogueMap extends Actor {
     /* Contains indices of Tile instances according to position in map */
@@ -18,7 +20,7 @@ public class RogueMap extends Actor {
     private final ArrayList<Body> bodies;
 
     public RogueMap(int mapXSize, int mapYSize) {
-        atlas = new TextureAtlas("tiles.atlas");
+        atlas = new TextureAtlas("assets/tiles.atlas");
         bodies = new ArrayList<Body>();
         generateMap(mapXSize, mapYSize);
     }
@@ -26,17 +28,27 @@ public class RogueMap extends Actor {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         for (Body body : bodies) {
-            batch.draw(
-                atlas.findRegion("stone"),
-                body.getPosition().x * RoguePG.PPM,
-                body.getPosition().y * RoguePG.PPM
-            );
+            batch.draw(atlas.findRegion("dirt"), body.getPosition().x * RoguePG.PPM, body.getPosition().y * RoguePG.PPM);
         }
     }
+
     private void generateMap(int mapXSize, int mapYSize) {
         tileIndices = new int[mapXSize][mapYSize];
         createConnections(createRooms(mapXSize, mapYSize));
         generateMapBodies();
+    }
+
+    public static void printMap() {
+        if (tileIndices != null) {
+            for (int i = tileIndices.length - 1; i >= 0; i--) {
+                for (int j = 0; j < tileIndices.length; j++) {
+                    String p = ((tileIndices[j][i] != 1) ? tileIndices[j][i] + "" : " ");
+                    System.out.print("|" + p);
+                }
+                System.out.print("|");
+                System.out.println();
+            }
+        }
     }
 
     public ArrayList<Rectangle> createRooms(int mapXSize, int mapYSize) {
@@ -45,16 +57,15 @@ public class RogueMap extends Actor {
          * the map by setting zeroes to other tile numbers.
          */
 
-        /* Rooms */
         Random random = new Random();
         ArrayList<Rectangle> roomArray = new ArrayList<Rectangle>();
 
-        OUTER:
-        for (int tries = 100; tries > 0; tries--) {
-            Rectangle room = new Rectangle(1 + random.nextInt(mapXSize-1), 1 + random.nextInt(mapYSize-1), random.nextInt(5 - 2) + 2, random.nextInt(5 - 2) + 2);
+        OUTER: for (int tries = 100; tries > 0; tries--) {
+            Rectangle room = new Rectangle(1 + random.nextInt(mapXSize - 1), 1 + random.nextInt(mapYSize - 1),
+                    2 + random.nextInt(5 - 2), 2 + random.nextInt(5 - 2));
             /*
-             * Check if this room would touch or go over the map boundaries.
-             * If so, skip this iteration.
+             * Check if this room would touch or go over the map boundaries. If so, skip this
+             * iteration.
              */
             if (room.x + room.width > mapXSize - 2 || room.y + room.height > mapYSize - 2) {
                 continue;
@@ -80,22 +91,22 @@ public class RogueMap extends Actor {
         }
         return roomArray;
     }
-    public void createConnections(ArrayList<Rectangle> roomArray){
-        if(tileIndices[0][0] == Tile.STONE.ordinal()){
-            return;
-        }
-        /* Connections */
+
+    public void createConnections(ArrayList<Rectangle> roomArray) {
         Random random = new Random();
         for (int i = 0; i < 10; i++) {
-            
+
             // from + rndGenerator.nextInt(to - from + 1)
             Rectangle startRoom = roomArray.get(random.nextInt(roomArray.size() - 1));
             Rectangle endRoom = roomArray.get(random.nextInt(roomArray.size() - 1));
 
+            // Middle of the first room
             int startX = (int) (startRoom.width / 2 + startRoom.x);
             int startY = (int) (startRoom.height / 2 + startRoom.y);
+            // Middle of the second room
             int endX = (int) (endRoom.width / 2 + endRoom.x);
             int endY = (int) (endRoom.height / 2 + endRoom.y);
+
             // Randomly choose between x and y axis, 50:50 chance
             boolean moveInX = (random.nextDouble() >= 0.5f);
 
@@ -107,16 +118,17 @@ public class RogueMap extends Actor {
                 if (moveInX) {
                     if (startX < endX) {
                         startX++;
-                    } else if (startX > endX) {
+                    } else {
                         startX--;
                     }
                 } else {
                     if (startY < endY) {
                         startY++;
-                    } else if (startY > endY) {
+                    } else {
                         startY--;
                     }
                 }
+                // Chance to change direction again is 10%
                 if (random.nextDouble() < 0.1f) {
                     moveInX = !moveInX;
                 }
@@ -129,15 +141,17 @@ public class RogueMap extends Actor {
         bodyDef.type = BodyDef.BodyType.StaticBody;
         ChainShape shape = new ChainShape();
         /* Set vertices of the box in counter-clockwise order. */
-        float[] vertices = {0, 0, 1, 0, 1, 1, 0, 1};
+        float[] vertices = { 0, 0, 1, 0, 1, 1, 0, 1 };
         shape.createLoop(vertices);
 
         for (int i = 0; i < RogueMap.tileIndices.length; i++) {
             for (int j = 0; j < RogueMap.tileIndices[0].length; j++) {
                 if (tileIndices[i][j] == 0) {
-                    bodyDef.position.set(i + 0.5f, j + 0.5f);
+                    bodyDef.position.set(i, j);
+                    // bodyDef.position.set(i + 0.5f, j + 0.5f); This could cause bodies to spawn in
+                    // unpassable areas, needs further testing
                     Body body = RoguePG.world.createBody(bodyDef);
-                    body.createFixture(shape, 1);
+                    body.createFixture(shape, 0);
                     bodies.add(body);
                 }
             }
